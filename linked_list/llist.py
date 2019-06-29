88510c2be4e25c4b893ecdcaa1e3d648d601a98c
Chuncheng Ji <chuncheng.ji@ihandysoft.com>
2019-06-29 19:55:25 +0800

    add cycle linked list

--- linked_list/llist.py
+++ linked_list/llist.py
@@ -90,3 +90,48 @@ class LListWithRear(LList):
         p.next = None
         self._rear = p
         return e
+
+
+# 循环单链表
+class LCList:
+
+    def __init__(self):
+        self._rear = None
+
+    def is_empty(self):
+        return self._rear is None
+
+    # 前端插入
+    def prepend(self, elem):
+        p = LNode(elem)
+        if self._rear is None:
+            p.next = p
+            self._rear = p
+        else:
+            p.next = self._rear.next
+            self._rear.next = p
+
+    def append(self, elem):
+        self.prepend(elem)
+        self._rear = self._rear.next
+
+    # 首端弹出
+    def pop(self):
+        if self._rear is None:
+            raise ValueError('linkedlist no node')
+        p = self._rear.next
+        if self._rear is p:
+            self._rear = None
+        else:
+            self._rear.next = p.next
+        return p.elem
+
+    def print_all(self):
+        if self.is_empty():
+            return
+        p = self._rear.next
+        while True:
+            print(p.elem)
+            if p is self._rear:
+                break
+            p = p.next
