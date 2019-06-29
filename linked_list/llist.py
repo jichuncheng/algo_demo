6f97725d262e13e5c806edf0e0511448e235665b
Chuncheng Ji <chuncheng.ji@ihandysoft.com>
2019-06-29 17:39:17 +0800

    rename

--- linked_list/llist.py
+++ linked_list/llist.py
@@ -5,6 +5,7 @@ class LNode:
         self.next = next_
 
 
+# 单链表
 class LList:
 
     def __init__(self):
@@ -51,4 +52,41 @@ class LList:
         return e
 
 
+# 有尾节点的单链表
+class LListWithRear(LList):
 
+    def __init__(self):
+        LList.__init__(self)
+        self._rear = None
+
+    def prepend(self, elem):
+        if self._head is None:
+            self._head = LNode(elem)
+            self._rear = self._head
+        else:
+            self._head = LNode(elem, self._head)
+
+    def append(self, elem):
+        if self._head is None:
+            self._head = LNode(elem)
+            self._rear = self._head
+        else:
+            self._rear.next = LNode(elem)
+            self._rear = self._rear.next
+
+    def pop_last(self):
+        if self._head is None:
+            raise ValueError('linkedlist no node')
+        p = self._head
+        # 表中只有一个元素
+        if p.next is None:
+            e = p.elem
+            self._head = None
+            return e
+        # 知道p.next是最后节点
+        while p.next.next is not None:
+            p = p.next
+        e = p.next.elem
+        p.next = None
+        self._rear = p
+        return e
